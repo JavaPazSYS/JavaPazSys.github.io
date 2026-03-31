1e8f26a56f4cbca3e0b46af383bc8d9003646d30
JavieraPaz <jp.becerradiaz@gmail.com>
2026-03-31 17:53:24 -0300

    update cv

--- JavaSyS/src/app/home/home.component.ts
+++ JavaSyS/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-home',
@@ -7,6 +7,19 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  emailLink: string = 'https://mail.google.com/mail/?view=cm&fs=1&to=jp.becerradiaz@gmail.com';
 
+  ngOnInit() {
+    this.checkDevice();
+  }
+
+  checkDevice() {
+    if (typeof navigator !== 'undefined') {
+      const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+      if (isMobile) {
+        this.emailLink = 'mailto:jp.becerradiaz@gmail.com';
+      }
+    }
+  }
 }
